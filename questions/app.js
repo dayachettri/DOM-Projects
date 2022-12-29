e5995c26f477cad3a25ec02a26111489fb990fde
Daya Chettri <Dayasagar073@gmail.com>
2022-12-30 01:06:08 +0530

    add finished code

--- questions/app.js
+++ questions/app.js
@@ -1,2 +1,25 @@
-//using selectors inside the element
-// traversing the dom
+//#using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', () => {
+    questions.forEach(item => {
+      // if the question which i clicked on is not the same as the question in line 5 run the below code
+      item !== question && item.classList.remove('show-text');
+    });
+    question.classList.toggle('show-text');
+  });
+});
+
+//# traversing the dom
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(btn => {
+//   btn.addEventListener('click', e => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
